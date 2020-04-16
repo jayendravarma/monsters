047da87535c3e3b7955eb141b72351b01c478fa7
jayendra11 <jayendra.varma11@gmail.com>
2020-04-16 18:25:00 +0530

    Updates

---
+++ precache-manifest.37005bbd342599678c8a7a457d5f440e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfc5e3351bf0aa998137a69f962788e",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "42dd07f0fd9202b7ac18",
+    "url": "/monsters/static/css/main.afbd53b9.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42dd07f0fd9202b7ac18",
+    "url": "/monsters/static/js/main.42fec623.chunk.js"
+  },
+  {
+    "revision": "341975cc8ff21a216c4c",
+    "url": "/monsters/static/js/runtime-main.7edf1021.js"
+  }
+]);
